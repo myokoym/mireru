ca7b7fb1abff1206bd6df3e3304f4dd22cbbd249
Masafumi Yokoyama <myokoym@gmail.com>
2013-04-07 22:12:12 +0900

    Support code highlight

--- lib/mireru/widget.rb
+++ lib/mireru/widget.rb
@@ -8,12 +8,24 @@ module Mireru
         image = Gtk::Image.new
         image.file = file
         widget = image
-      when /\A\.(txt|rb)\z/i
+      when /\A\.(txt)\z/i
         buffer = Gtk::TextBuffer.new
         buffer.text = File.open(file).read
         text = Gtk::TextView.new(buffer)
         text.editable = false
         widget = text
+      when /\A\.(rb)\z/i
+        require 'gtksourceview3'
+        buffer = Gtk::SourceBuffer.new
+        buffer.text = File.open(file).read
+        view = Gtk::SourceView.new(buffer)
+        view.show_line_numbers = true
+        lang = Gtk::SourceLanguageManager.new.get_language('ruby')
+        view.buffer.language = lang
+        view.buffer.highlight_syntax = true
+        view.buffer.highlight_matching_brackets = true
+        view.editable = false
+        widget = view
       else
         raise "coding error: uncheck file type."
       end
--- mireru.gemspec
+++ mireru.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_runtime_dependency("gtk3")
+  spec.add_runtime_dependency("gtksourceview3")
 
   spec.add_development_dependency("test-unit")
   spec.add_development_dependency("test-unit-notify")
