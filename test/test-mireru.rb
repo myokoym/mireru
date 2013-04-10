dbf9593912275cb3bfe1b9bbb74b1c7fe6ee792e
Masafumi Yokoyama <myokoym@gmail.com>
2013-04-10 11:23:39 +0900

    test: add help option test case

--- test/test-mireru.rb
+++ test/test-mireru.rb
@@ -1,4 +1,23 @@
 require "mireru/command/mireru"
 
 class MireruTest < Test::Unit::TestCase
+  def setup
+    @mireru = Mireru::Command::Mireru.new
+  end
+
+  def test_run_help_option
+    arguments = %w(--help)
+    mock(@mireru).write_help_message
+    assert_raise SystemExit do
+      @mireru.run(arguments)
+    end
+  end
+
+  def test_run_help_option_sugar
+    arguments = %w(-h)
+    mock(@mireru).write_help_message
+    assert_raise SystemExit do
+      @mireru.run(arguments)
+    end
+  end
 end
