97e6f467a8f984e82b3add22a959def41fa6244f
Masafumi Yokoyama <myokoym@gmail.com>
2013-10-14 23:05:04 +0900

    middleman: support localization (i18n)

--- middleman/config.rb
+++ middleman/config.rb
@@ -51,6 +51,9 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+# Localization
+activate :i18n
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
