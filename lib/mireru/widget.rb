6a10151229537d36e00817c0456dab377745d960
Masafumi Yokoyama <myokoym@gmail.com>
2014-01-09 22:23:11 +0900

    Unindent

--- lib/mireru/widget.rb
+++ lib/mireru/widget.rb
@@ -7,29 +7,29 @@ require "mireru/widget/binary"
 module Mireru
   module Widget
     module_function
-      def create(file, width=10000, height=10000)
-        if image?(file)
-          widget = Mireru::Image.create(file)
-        elsif video?(file)
-          widget = Mireru::Video.create(file)
-        elsif text?(file)
-          widget = Mireru::Text.create(file)
-        else
-          widget = Mireru::Binary.create(file)
-        end
-        widget
+    def create(file, width=10000, height=10000)
+      if image?(file)
+        widget = Mireru::Image.create(file)
+      elsif video?(file)
+        widget = Mireru::Video.create(file)
+      elsif text?(file)
+        widget = Mireru::Text.create(file)
+      else
+        widget = Mireru::Binary.create(file)
       end
+      widget
+    end
 
-      def image?(file)
-        /\.(png|jpe?g|gif|ico|ani|bmp|pnm|ras|tga|tiff|xbm|xpm)\z/i =~ file
-      end
+    def image?(file)
+      /\.(png|jpe?g|gif|ico|ani|bmp|pnm|ras|tga|tiff|xbm|xpm)\z/i =~ file
+    end
 
-      def video?(file)
-        /\.(ogm|mp4|flv|mpe?g2?|ts|mov|avi|divx|mkv|wmv|asf|wmx)\z/i =~ file
-      end
+    def video?(file)
+      /\.(ogm|mp4|flv|mpe?g2?|ts|mov|avi|divx|mkv|wmv|asf|wmx)\z/i =~ file
+    end
 
-      def text?(file)
-        File.read(file).valid_encoding?
-      end
+    def text?(file)
+      File.read(file).valid_encoding?
+    end
   end
 end
