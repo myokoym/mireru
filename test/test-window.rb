267e069a8fb77b3d16fa6d8e69d5f89110dfc05d
Masafumi Yokoyama <myokoym@gmail.com>
2014-03-11 21:54:33 +0900

    test: indent

--- test/test-window.rb
+++ test/test-window.rb
@@ -13,27 +13,27 @@ class WindowTest < Test::Unit::TestCase
   end
 
   class AddFromFileTest
-  def test_add_from_file_of_scrollable
-    file = __FILE__
-    mock(Mireru::Widget).create(file, *@window.size) do
-      Gtk::TextView.new
+    def test_add_from_file_of_scrollable
+      file = __FILE__
+      mock(Mireru::Widget).create(file, *@window.size) do
+        Gtk::TextView.new
+      end
+      mock(@window).show_all
+      @window.add_from_file(file)
+      assert_equal(Gtk::ScrolledWindow, @window.child.class)
+      assert_equal(Gtk::TextView, @window.child.child.class)
     end
-    mock(@window).show_all
-    @window.add_from_file(file)
-    assert_equal(Gtk::ScrolledWindow, @window.child.class)
-    assert_equal(Gtk::TextView, @window.child.child.class)
-  end
 
-  def test_add_from_file_of_no_scrollable
-    file = "fixtures/nijip.png"
-    mock(Mireru::Widget).create(file, *@window.size) do
-      Gtk::Image.new
+    def test_add_from_file_of_no_scrollable
+      file = "fixtures/nijip.png"
+      mock(Mireru::Widget).create(file, *@window.size) do
+        Gtk::Image.new
+      end
+      mock(@window).show_all
+      @window.add_from_file(file)
+      assert_equal(Gtk::ScrolledWindow, @window.child.class)
+      assert_equal(Gtk::Viewport, @window.child.child.class)
+      assert_equal(Gtk::Image, @window.child.child.child.class)
     end
-    mock(@window).show_all
-    @window.add_from_file(file)
-    assert_equal(Gtk::ScrolledWindow, @window.child.class)
-    assert_equal(Gtk::Viewport, @window.child.child.class)
-    assert_equal(Gtk::Image, @window.child.child.child.class)
-  end
   end
 end
