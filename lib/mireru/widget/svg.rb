b70ea9f4bad688c2a9c8bbebec08e292682b9882
Masafumi Yokoyama <myokoym@gmail.com>
2014-07-09 23:38:42 +0900

    Unindent

--- lib/mireru/widget/svg.rb
+++ lib/mireru/widget/svg.rb
@@ -11,17 +11,17 @@ module Mireru
 
       def initialize(file)
         super()
-          handle = RSVG::Handle.new_from_file(file)
-          width, height = handle.dimensions.to_a
-          set_size_request(width, height)
+        handle = RSVG::Handle.new_from_file(file)
+        width, height = handle.dimensions.to_a
+        set_size_request(width, height)
 
-          signal_connect("draw") do |widget, event|
-            context = widget.window.create_cairo_context
-            context.save do
-              context.render_rsvg_handle(handle)
-            end
-            true
+        signal_connect("draw") do |widget, event|
+          context = widget.window.create_cairo_context
+          context.save do
+            context.render_rsvg_handle(handle)
           end
+          true
+        end
       end
     end
   end
