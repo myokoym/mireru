3dd734628e9c0a580f1bf3d2215fca70f7877074
Masafumi Yokoyama <myokoym@gmail.com>
2014-07-09 23:35:10 +0900

    Unindent

--- lib/mireru/widget/pdf.rb
+++ lib/mireru/widget/pdf.rb
@@ -5,21 +5,21 @@ module Mireru
     class PDF < Gtk::DrawingArea
       def initialize(file)
         super()
-          document = Poppler::Document.new(file)
-          width, height = document.first.size
-          set_size_request(width, height * document.size)
+        document = Poppler::Document.new(file)
+        width, height = document.first.size
+        set_size_request(width, height * document.size)
 
-          signal_connect("draw") do |widget, event|
-            context = widget.window.create_cairo_context
-            document.each_with_index do |page, i|
-              context.save do
-                context.translate(0, height * i)
-                context.render_poppler_page(document[i])
-              end
+        signal_connect("draw") do |widget, event|
+          context = widget.window.create_cairo_context
+          document.each_with_index do |page, i|
+            context.save do
+              context.translate(0, height * i)
+              context.render_poppler_page(document[i])
             end
-            context.show_page
-            true
           end
+          context.show_page
+          true
+        end
       end
 
       class << self
