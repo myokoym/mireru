eb01223312a9bae97168084cdafe727b6cb1d7b4
Masafumi Yokoyama <myokoym@gmail.com>
2014-07-11 00:33:37 +0900

    Don't pass needless arguments

--- lib/mireru/widget/image.rb
+++ lib/mireru/widget/image.rb
@@ -4,7 +4,7 @@ module Mireru
   module Widget
     class Image < Gtk::Image
       def initialize(file, width, height)
-        super
+        super()
         pixbuf = Gdk::Pixbuf.new(file)
         if pixbuf.width > width || pixbuf.height > height
           pixbuf = Gdk::Pixbuf.new(file, width, height)
