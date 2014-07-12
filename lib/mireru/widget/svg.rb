66301bb62356e6d4f67d845b1202fa2028c8ddd5
Masafumi Yokoyama <myokoym@gmail.com>
2014-07-12 10:38:15 +0900

    svg: Fit a parent widget

--- lib/mireru/widget/svg.rb
+++ lib/mireru/widget/svg.rb
@@ -8,10 +8,20 @@ module Mireru
         super()
         handle = RSVG::Handle.new_from_file(file)
         width, height = handle.dimensions.to_a
-        set_size_request(width, height)
 
         signal_connect("draw") do |widget, event|
           context = widget.window.create_cairo_context
+          window_width = widget.allocated_width
+          window_height = widget.allocated_height
+          width_scale = window_width.to_f / width
+          height_scale = window_height.to_f / height
+          scale = [width_scale, height_scale].min
+          begin
+            context.scale(scale, scale)
+          rescue => e
+            $stderr.puts("#{e.class}: #{e.message}")
+            $stderr.puts(e.backtrace)
+          end
           context.render_rsvg_handle(handle)
           true
         end
