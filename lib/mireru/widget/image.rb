37c535b56ff63e9e37313c77bb24ae4ac0a174a0
Masafumi Yokoyama <myokoym@gmail.com>
2014-07-13 00:23:27 +0900

    image: Enable animation for image files such as animated GIF

--- lib/mireru/widget/image.rb
+++ lib/mireru/widget/image.rb
@@ -5,11 +5,16 @@ module Mireru
     class Image < Gtk::Image
       def initialize(file, width, height)
         super()
-        pixbuf = Gdk::Pixbuf.new(file)
-        if pixbuf.width > width || pixbuf.height > height
-          pixbuf = Gdk::Pixbuf.new(file, width, height)
+        pixbuf_animation = Gdk::PixbufAnimation.new(file)
+        if pixbuf_animation.static_image?
+          pixbuf = pixbuf_animation.static_image
+          if pixbuf.width > width || pixbuf.height > height
+            pixbuf = Gdk::Pixbuf.new(file, width, height)
+          end
+          self.pixbuf = pixbuf
+        else
+          self.pixbuf_animation = pixbuf_animation
         end
-        self.pixbuf = pixbuf
       end
     end
   end
