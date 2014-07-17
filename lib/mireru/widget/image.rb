4de9d807fafa987a166ddf136483e48ff66319e1
Masafumi Yokoyama <myokoym@gmail.com>
2014-07-18 00:46:12 +0900

    image: Introduce wrapper for static and animated

--- lib/mireru/widget/image.rb
+++ lib/mireru/widget/image.rb
@@ -5,23 +5,24 @@ module Mireru
     class Image < Gtk::Image
       def initialize(file, width, height)
         super()
-        pixbuf_animation = Gdk::PixbufAnimation.new(file)
-        if pixbuf_animation.static_image?
-          self.pixbuf = pixbuf_animation.static_image
-          if pixbuf.width > width || pixbuf.height > height
+        @pixbuf = Gdk::PixbufAnimation.new(file)
+        if @pixbuf.static_image?
+          @pixbuf = @pixbuf.static_image
+          if @pixbuf.width > width || @pixbuf.height > height
             scale_preserving_aspect_ratio(width, height)
           end
+          self.pixbuf = @pixbuf
         else
-          self.pixbuf_animation = pixbuf_animation
+          self.pixbuf_animation = @pixbuf
         end
       end
 
       def scale_preserving_aspect_ratio(width, height)
-        x_ratio = width.to_f / pixbuf.width
-        y_ratio = height.to_f / pixbuf.height
+        x_ratio = width.to_f / @pixbuf.width
+        y_ratio = height.to_f / @pixbuf.height
         ratio = [x_ratio, y_ratio].min
-        self.pixbuf = pixbuf.scale(pixbuf.width * ratio,
-                                   pixbuf.height * ratio)
+        self.pixbuf = @pixbuf.scale(@pixbuf.width * ratio,
+                                    @pixbuf.height * ratio)
       end
     end
   end
